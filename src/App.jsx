62e01b90b636a560ac0544aad2d57c379c58598c
Abbishek Kamak <aram0057@student.monash.edu>
2025-07-30 10:24:35 +1000

    about section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react"
 import Hero from "./Hero.jsx"
+import About from "./About.jsx";
 
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overlflow-x-hidden">
       <Hero />
+      <About/>
       
-      <section className = "z-0 min-h-screen bg-blue-500"/>
       
   
     </main>
